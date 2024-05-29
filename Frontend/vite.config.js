703c521834a5cdfac18a19adaacea963317a9a36
Irfan <irfanyusufk13@gmail.com>
2024-05-30 00:00:47 +0700

    adding docker

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,14 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Forward requests starting with '/api' to the backend
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+  server:{
+    host:"0.0.0.0",
+    port:5173,
   }
 });
